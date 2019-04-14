022cf22b7e3582b9ba33ba63944e7144c7daf385
mitchellgiles <47744016+mitchellgilesdev@users.noreply.github.com>
2019-04-14 22:06:14 +0800

    prettier

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,23 +12,22 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
   }
-  }
-  
-                  render() {
-             const status = "Next player: X";
 
-            return (
-          <div>
-              <div className="status">{status}</div>
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
         <div className="board-row">
-              {this.renderSquare(0)}
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
-                {this.renderSquare(2)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-    {this.renderSquare(3)}
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
-           {this.renderSquare(5)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
           {this.renderSquare(6)}
